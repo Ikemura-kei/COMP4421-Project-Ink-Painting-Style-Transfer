91f1a3085f5da0bdf253a89a70bbd1542bb9c19b
Ikemura-kei <ikemurakei2001@gmail.com>
2021-10-05 22:26:39 +0900

    finished test data preprocessing

---
+++ Research/ikemura-research/load_image.py
@@ -0,0 +1,15 @@
+
+# load all images in a directory into memory
+def load_images(path, size=(256,512)):
+	src_list, tar_list = list(), list()
+	# enumerate filenames in directory, assume all are images
+	for filename in listdir(path):
+		# load and resize the image
+		pixels = load_img(path + filename, target_size=size)
+		# convert to numpy array
+		pixels = img_to_array(pixels)
+		# split into satellite and map
+		sat_img, map_img = pixels[:, :256], pixels[:, 256:]
+		src_list.append(sat_img)
+		tar_list.append(map_img)
+	return [asarray(src_list), asarray(tar_list)]
